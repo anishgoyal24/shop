c8901cf29d483b3babbf276868430bfeca217a00
Anish Goyal <anishgoyal2406@gmail.com>
2019-06-14 15:51:57 +0530

    fixes

---
+++ src/main/java/com/app/shop/controllers/customer/OrderController.java
@@ -0,0 +1,29 @@
+package com.app.shop.controllers.customer;
+
+import com.app.shop.entity.OrderHeader;
+import com.app.shop.services.customer.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping(value = "/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping(value = "/check")
+    @PreAuthorize("hasAnyAuthority('ROLE_party')")
+    public HashMap<String, Object> checkOrder(@RequestBody OrderHeader orderHeader){
+        return orderService.checkStock(orderHeader);
+    }
+
+    @PostMapping(value = "/place")
+    @PreAuthorize("hasAnyAuthority('ROLE_party')")
+    public HashMap<String, Object> placeOrder(@RequestBody OrderHeader orderHeader){
+        return orderService.placeOrder(orderHeader);
+    }
+}
--- src/main/java/com/app/shop/services/customer/OrderService.java
+++ src/main/java/com/app/shop/services/customer/OrderService.java
@@ -1,10 +1,8 @@
 package com.app.shop.services.customer;
 
-import com.app.shop.entity.ItemPackingDetails;
 import com.app.shop.entity.OrderDetail;
 import com.app.shop.entity.OrderHeader;
 import com.app.shop.repository.customer.DetailsRepository;
-import com.app.shop.repository.customer.OrderRepository;
 import com.app.shop.repository.customer.PartyStockRepository;
 import com.app.shop.utils.OrderBookingThread;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +12,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class OrderService {
@@ -23,6 +20,8 @@ public class OrderService {
     private DetailsRepository detailsRepository;
     @Autowired
     private PartyStockRepository partyStockRepository;
+    @Autowired
+    private ProductService productService;
     private HashMap<String, Object> returnObject;
 
     public HashMap<String, Object> placeOrder(OrderHeader orderHeader){
@@ -48,20 +47,23 @@ public class OrderService {
         return orderId;
     }
 
-    private HashMap<String, Object> checkStock(OrderHeader orderHeader){
+    public HashMap<String, Object> checkStock(OrderHeader orderHeader){
         returnObject = new HashMap<>();
         HashMap<Integer, Integer> outOfStock = new HashMap<>();
+        ArrayList<Float> discount = new ArrayList<>();
         for (OrderDetail orderDetails: orderHeader.getOrderDetails()){
             Object[][] objects = partyStockRepository.findStockAndPrice(orderHeader.getPartyDetails().getState(), orderDetails.getItemDetails().getId());
             if ((int)objects[0][0] < orderDetails.getQuantity()){
                 outOfStock.put(orderDetails.getItemDetails().getId(), (int)objects[0][0]);
             }
+            discount.add((float)productService.getDiscount(orderDetails.getItemDetails().getId()).get("discount"));
             orderDetails.setActualCost((int)objects[0][1]);
         }
         if (outOfStock.size()>0){
             returnObject.put("message", "some items out of stock");
             returnObject.put("out of stock items", outOfStock);
         }
+        returnObject.put("discounts", discount);
         returnObject.put("updated price", orderHeader);
         return returnObject;
     }
