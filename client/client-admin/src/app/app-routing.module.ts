bf9c21dc5e322ebf68d5d6d8768da1cee0653db0
shubham3597 <vshubham5636@gmail.com>
2019-09-07 16:24:13 +0530

    preparing the UI of account administration

--- client/client-admin/src/app/app-routing.module.ts
+++ client/client-admin/src/app/app-routing.module.ts
@@ -6,6 +6,13 @@ import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './authentication/signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { OverviewComponent } from './dashboard/overview/overview.component';
+import { StocksComponent } from './dashboard/stocks/stocks.component';
+import { ProductsComponent } from './dashboard/products/products.component';
+import { CategoriesComponent } from './dashboard/categories/categories.component';
+import { AccountComponent } from './dashboard/account/account.component';
+import { NewAccountsComponent } from './dashboard/account/new-accounts/new-accounts.component';
+import { ManageAccountsComponent } from './dashboard/account/manage-accounts/manage-accounts.component';
+import { RolesComponent } from './dashboard/account/roles/roles.component';
 
 /**
  * Signup Component is being used in the dashboard section only
@@ -15,10 +22,20 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
+  { path: 'signup', component: NewAccountsComponent},
   { path: 'dashboard', component: DashboardComponent, 
     children: [
-      { path: 'overview', component: OverviewComponent }
+      { path: 'overview', component: OverviewComponent },
+      { path: 'stocks', component: StocksComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'categories', component: CategoriesComponent },
+      { path: 'accounts', component: AccountComponent,
+        children: [
+          { path: 'new', component: NewAccountsComponent },
+          { path: 'manage', component: ManageAccountsComponent } ,
+          { path: 'roles', component: RolesComponent },
+        ]
+      }
     ]
   },
 ];
