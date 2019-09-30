9a309db9c5d7f7ff54b5da1afb6352ad5de5beaf
Anish Goyal <anishgoyal2406@gmail.com>
2019-09-30 23:10:01 +0530

    category completed

--- src/main/java/com/app/shop/utils/Startup.java
+++ src/main/java/com/app/shop/utils/Startup.java
@@ -1,10 +1,14 @@
 package com.app.shop.utils;
 
 import com.app.shop.entity.EmployeeDetails;
+import com.app.shop.entity.UserDetails;
+import com.app.shop.repository.common.UserAuthRepository;
 import com.app.shop.services.employee.EmployeeDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +16,8 @@ public class Startup {
 
     @Autowired
     private EmployeeDetailsService employeeDetailsService;
+    @Autowired
+    private UserAuthRepository userAuthRepository;
 
     @EventListener(ApplicationReadyEvent.class)
     public void addAdminStartup(ApplicationReadyEvent applicationReadyEvent){
@@ -24,5 +30,6 @@ public class Startup {
         defaultAdmin.setState("Haryana");
         defaultAdmin.setPrimaryPhone("7027770150");
         employeeDetailsService.addNewEmployee(defaultAdmin);
+        userAuthRepository.save(new UserDetails("Parveen Goel", new BCryptPasswordEncoder().encode("admin"), 1, "admin"));
     }
 }
