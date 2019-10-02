97f1ebc9eb1c1d6a037bb78c20d462e90619ffd8
shubham3597 <vshubham5636@gmail.com>
2019-10-02 16:29:02 +0530

    pushing the login component

--- client/client-admin/src/app/authentication/login/login.component.ts
+++ client/client-admin/src/app/authentication/login/login.component.ts
@@ -3,6 +3,8 @@ import { Location } from '@angular/common';
 import { NgxUiLoaderService, Loader, SPINNER } from 'ngx-ui-loader';
 import { Router } from '@angular/router';
 import { AdminService } from 'src/shared/services/admin.service';
+import { SnotifyService, SnotifyPosition, SnotifyToastConfig } from 'ng-snotify';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -15,10 +17,13 @@ export class LoginComponent implements OnInit {
     private _location: Location,
     private ngxService: NgxUiLoaderService,
     private router: Router,
-    private adminService: AdminService) { }
+    private adminService: AdminService,
+    private snotifyService: SnotifyService) { }
 
   userName: String;
   password: String;
+
+  isLoading$ = new BehaviorSubject(false);
 
   ngOnInit() {
     this.ngxService.start()
@@ -37,19 +42,23 @@ export class LoginComponent implements OnInit {
         username: this.userName,
         password: this.password
       }
-
       if (userData) {
+        this.isLoading$.next(true);
         this.adminService.authenticate(userData)
           .subscribe((res) => {
-            //console.log('Successfully Logged In', res);
-            // if(res.headers.get('Authorization')){
-            //   this.router.navigate(['/dashboard', 'overview']);
-            // }
-            console.log(res.headers.keys())
-             
+            //console.log('Successfully Logged In', res.headers.keys());
+            if (res.headers.get('Authorization')) {
+              //this.snotifyService.success('Example body content');
+              localStorage.setItem("token", res.headers.get('Authorization').split(" ")[1]);
+              this.isLoading$.next(false);
+              this.router.navigate(['/dashboard', 'overview']);
+            }
+
           }, (err) => {
             console.log('HTTP Response Error', err);
           })
+
+
       }
 
     } catch (err) {
