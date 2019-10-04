6eb2189929df54c5dfdc062cd5a3f03864497c51
Anish Goyal <anishgoyal2406@gmail.com>
2019-10-04 20:57:43 +0530

    categories change

--- src/main/java/com/app/shop/controllers/employee/CategoryController.java
+++ src/main/java/com/app/shop/controllers/employee/CategoryController.java
@@ -5,15 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 
-@RestController(value = "/category")
+@RestController
+@RequestMapping(value = "/category")
 public class CategoryController {
     Logger logger = LoggerFactory.getLogger(CategoryController.class);
 
