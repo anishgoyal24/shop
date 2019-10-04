49653aee1bfe99bb0cd6dc58239d897e3bb1b8eb
Anish Goyal <anishgoyal2406@gmail.com>
2019-10-04 20:51:47 +0530

    categories change

--- src/main/java/com/app/shop/ShopApplication.java
+++ src/main/java/com/app/shop/ShopApplication.java
@@ -1,11 +1,14 @@
 package com.app.shop;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
+@ComponentScan(basePackages = {"com.app.shop"})
 public class ShopApplication {
 
 	public static void main(String[] args) {
