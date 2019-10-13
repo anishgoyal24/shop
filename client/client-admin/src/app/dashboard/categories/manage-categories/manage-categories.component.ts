6e8e45d168986fa7050047a46e9db1ecc4b581c5
shubham3597 <vshubham5636@gmail.com>
2019-10-13 17:17:01 +0530

    edit category completed

--- client/client-admin/src/app/dashboard/categories/manage-categories/manage-categories.component.ts
+++ client/client-admin/src/app/dashboard/categories/manage-categories/manage-categories.component.ts
@@ -18,10 +18,22 @@ export class ManageCategoriesComponent implements OnInit {
 
   listLength = -1;
 
+  editStatus = false;
+
+  categoryModel={
+    category: '',
+    description:'',
+    id: ''
+  }
+
   isLoadingQuery$ = new BehaviorSubject(false);
 
   async ngOnInit() {
     await this.getAllCategories();
+  }
+
+  firstLetterCaptialise(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   async getAllCategories() {
@@ -30,8 +42,11 @@ export class ManageCategoriesComponent implements OnInit {
       return new Promise((resolve, reject) => {
         this.categoryService.getAllCategories()
           .subscribe((res) => {
-            console.log(res);
+            // console.log(res);
             this.categoriesList = res['data'];
+            res['data'].forEach(element => {
+              element.category = this.firstLetterCaptialise(element.category);
+            });
             this.listLength = res['data'].length;
             this.ngxService.stopBackground();
             resolve();
@@ -62,7 +77,7 @@ export class ManageCategoriesComponent implements OnInit {
               this.categoryService.deleteCategory(categoryData)
               .subscribe((res)=>{
                 this.categoriesList.splice(index, 1);
-                console.log('Category Deleted', res);
+                // console.log('Category Deleted', res);
                 this.listLength = this.categoriesList.length;
                 this.ngxService.stopBackground();
                 this.snotifyService.info("Category removed!");
@@ -90,11 +105,13 @@ export class ManageCategoriesComponent implements OnInit {
       if(this.listLength > 0){
         this.ngxService.startBackground();
         return new Promise((resolve, reject) => {
-          this.categoryService.searchCategory(categoryName)
+          this.categoryService.searchCategory(categoryName.toLowerCase())
           .subscribe((res)=>{
-            console.log('Categories Found', res);
+            // console.log('Categories Found', res);
             this.categoriesList = res['data'];
-  
+            res['data'].forEach(element => {
+              element.category = this.firstLetterCaptialise(element.category);
+            });
             if(res['data'].length == 0)
               this.isLoadingQuery$.next(true);
             else
@@ -114,6 +131,30 @@ export class ManageCategoriesComponent implements OnInit {
       console.log('Internal Server Error', err);
       this.isLoadingQuery$.next(false);
       this.ngxService.stopBackground();
+      this.snotifyService.error("Some internal server occured, kindly check after some time...");
+    }
+  }
+
+  async editCategory(categoryData, categoryObj){
+    try{
+      // console.log(categoryData, categoryObj);
+      categoryObj.category = categoryData.category;
+      categoryObj.description = categoryData.description;
+      categoryObj.id = categoryData.id;
+      return new Promise((resolve, reject)=>{
+        this.categoryService.editCategory(categoryData)
+        .subscribe((res)=>{
+          // console.log('Category Edited', res);
+          this.snotifyService.info("Category edited!");
+          resolve()
+        }, (err)=>{
+          console.log('Category not edited', err);
+          this.snotifyService.error("Failed to edit the category, please try again!");
+          reject(err);
+        })
+      })
+    } catch(err){
+      console.log('Internal Server Error', err);
       this.snotifyService.error("Some internal server occured, kindly check after some time...");
     }
   }
