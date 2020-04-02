7867fec78085f194ec2ae65184a7f0812b583faa
Anish Goyal <anishgoyal2406@gmail.com>
2020-04-02 20:34:18 +0530

    image fix

--- src/main/java/com/app/shop/services/employee/ProductManagementService.java
+++ src/main/java/com/app/shop/services/employee/ProductManagementService.java
@@ -116,7 +116,7 @@ public class ProductManagementService {
         if (image.isEmpty())return "failure";
         try{
             byte[] bytes = image.getBytes();
-            String UPLOADED_FOLDER="src/main/assets/images/product/";
+            String UPLOADED_FOLDER="src/main/resource/assets/images/product/";
             Path path = Paths.get(UPLOADED_FOLDER + name + ".jpg");
             Files.write(path, bytes);
             return "success";
