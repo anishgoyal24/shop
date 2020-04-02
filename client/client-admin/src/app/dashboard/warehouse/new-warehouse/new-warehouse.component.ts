322ecaac7fd9e0d9a0982346fd6412cb8d180175
Anish Goyal <anishgoyal2406@gmail.com>
2020-04-02 17:51:28 +0530

    warehouse add'

--- client/client-admin/src/app/dashboard/warehouse/new-warehouse/new-warehouse.component.ts
+++ client/client-admin/src/app/dashboard/warehouse/new-warehouse/new-warehouse.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { UtilityService } from 'src/shared/services/utility.service';
+import { WarehouseService } from 'src/shared/services/warehouse.service';
 
 @Component({
   selector: 'app-new-warehouse',
@@ -8,24 +10,28 @@ import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 })
 export class NewWarehouseComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private utilityService: UtilityService,
+    private warehouseService: WarehouseService
+  ) { }
   
   /**
    * Defining account details object for which a new account will be created
    */
-  accountDetails: {partyName: string, partyEmail: string, contactPerson: string,  primaryPhone: Number, secondaryPhone: Number, address: string, city: string, state: string, country: string, pincode: number, password: string, partyType: Number} = {
-    partyName: null,
-    partyEmail: null,
+  accountDetails: {warehouseName: string, warehouseEmail: string, personOfContact: string,  primaryPhone: Number, secondaryPhone: Number, address: string, city: string, state: string, country: string, pincode: number, password: string, status: string, role: string} = {
+    warehouseName: null,
+    warehouseEmail: null,
     primaryPhone: null,
     secondaryPhone: null,
-    contactPerson: null,
+    personOfContact: null,
     address: null,
     city: null,
     state: null,
     country: null,
     pincode: null,
     password: null,
-    partyType: null
+    status: null,
+    role: null
   }
 
   // partyHomeComponent = new PartyHomeComponent(this.partyService);
@@ -35,6 +41,19 @@ export class NewWarehouseComponent implements OnInit {
   public isLoading$ = new BehaviorSubject(false);
 
   ngOnInit() {
+  }
+
+  createWarehouse(accountDetails: any){
+    this.utilityService.asyncNotification('Creating New Warehouse', 
+    new Promise((resolve, reject)=>{
+      this.warehouseService.createNewWarehouse(accountDetails)
+      .then(()=>{
+        resolve(this.utilityService.resolveAsyncPromise('Warehouse Created!'))
+      })
+      .catch(()=>{
+        reject(this.utilityService.rejectAsyncPromise('Some error occured, while creating the warehouse!'))
+      })
+    }))
   }
 
 }
---
+++ client/client-admin/src/shared/services/warehouse.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WarehouseService {
+
+  constructor(private _http: HttpClient) { }
+
+  /**
+   * POST request to create a new warehouse
+   * @param warehouseDetails
+   */
+  createNewWarehouse(warehouseDetails: Object){
+    return this._http.post(environment.BASE_URL_API + '/warehouse/new', warehouseDetails).toPromise();
+  }
+
+}
