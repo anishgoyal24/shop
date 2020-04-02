ee5e063b4d9258eb6b691735a5ed046651c4d601
Anish Goyal <anishgoyal2406@gmail.com>
2020-04-02 20:43:00 +0530

    image fix

--- src/main/java/com/app/shop/ShopApplication.java
+++ src/main/java/com/app/shop/ShopApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 @ComponentScan(basePackages = {"com.app.shop"})
+@EnableCaching
 public class ShopApplication {
 
 	public static void main(String[] args) {
