7528daefb612eaaa712b654064a9e606fa6eff5a
Anish Goyal <anishgoyal2406@gmail.com>
2020-04-04 11:55:35 +0530

    api fix

--- src/main/java/com/app/shop/entity/ItemDetails.java
+++ src/main/java/com/app/shop/entity/ItemDetails.java
@@ -29,7 +29,7 @@ public class ItemDetails {
     private String description;
     @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
     @OneToMany(mappedBy = "itemDetails")
-    @Cascade({org.hibernate.annotations.CascadeType.SAVE_UPDATE})
+    @Cascade({org.hibernate.annotations.CascadeType.SAVE_UPDATE, org.hibernate.annotations.CascadeType.PERSIST})
     private List<ItemPackingDetails> itemPackingDetails;
     @Column(name = "customer_allowed")
     private String customerAllowed;
