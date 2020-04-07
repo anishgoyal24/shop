e5fe20ce1cb79eabd9934a4b4eba96af77dacec2
Anish Goyal <anishgoyal2406@gmail.com>
2020-04-07 15:59:08 +0530

    add packing fix

--- src/main/java/com/app/shop/entity/ItemStock.java
+++ src/main/java/com/app/shop/entity/ItemStock.java
@@ -17,7 +17,7 @@ public class ItemStock {
     @ManyToOne
     @JoinColumn(name = "product_id", referencedColumnName = "id")
     private ItemPackingDetails itemPackingDetails;
-    @ManyToOne(cascade = CascadeType.MERGE)
+    @ManyToOne
     @JoinColumn(name = "warehouse_id", referencedColumnName = "warehouse_id")
     private WarehouseDetails warehouseDetails;
     private int quantity;
