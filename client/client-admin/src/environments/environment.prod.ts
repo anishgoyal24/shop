d7b37685762c6906137c52b3810806b65f0bb6c8
Anish Goyal <anishgoyal2406@gmail.com>
2020-04-19 13:59:01 +0530

    changed server url

--- client/client-admin/src/environments/environment.prod.ts
+++ client/client-admin/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // BASE URL FOR SERVER API
-  BASE_URL_API: 'https://saifcoapp.herokuapp.com'
+  BASE_URL_API: 'https://http://34.93.241.26/api/main/',
+  ORDERS_API: 'http://34.93.241.26/api/orders/'
 };
--- client/client-user/src/environments/environment.prod.ts
+++ client/client-user/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL_API: 'https://http://34.93.241.26/api/main/',
+  ORDERS_API: 'http://34.93.241.26/api/orders/'
 };
