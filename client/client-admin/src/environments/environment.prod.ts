97cfc3bb3177fc8029b800b3ab69f4fac2bc9dbd
shubhamcommits <vshubham5636@gmail.com>
2020-04-20 16:16:15 +0530

    preparing the docker images

--- client/client-admin/src/environments/environment.prod.ts
+++ client/client-admin/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // BASE URL FOR SERVER API
-  BASE_URL_API: 'http://34.93.241.26/api/main/',
-  ORDERS_API: 'http://34.93.241.26/api/orders/'
+  BASE_URL_API: 'http://34.93.241.26/api/main',
+  ORDERS_API: 'http://34.93.241.26/api/orders'
 };
--- client/client-admin/src/environments/environment.ts
+++ client/client-admin/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  BASE_URL_API: 'http://34.93.241.26/api/main/',
-  ORDERS_API: 'http://34.93.241.26/api/orders/'
+  BASE_URL_API: 'http://34.93.241.26/api/main',
+  ORDERS_API: 'http://34.93.241.26/api/orders'
 };
 
 /*
