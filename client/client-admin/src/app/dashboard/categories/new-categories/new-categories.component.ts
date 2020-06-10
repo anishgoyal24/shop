2e9b0947e2f49a6aeb241dfa530c4bacfbc7f982
Shubham Singh <shubhamsingh@SHUBHAMs-MacBook-Pro.local>
2020-06-10 16:26:21 +0530

    pushing the new state for category

--- client/client-admin/src/app/dashboard/categories/new-categories/new-categories.component.ts
+++ client/client-admin/src/app/dashboard/categories/new-categories/new-categories.component.ts
@@ -21,34 +21,39 @@ export class NewCategoriesComponent implements OnInit {
   }
 
   async createNewCategory() {
-    try {
-      this.utilityService.asyncNotification('Creating new category, please wait...', new Promise((resolve, reject) => {
-        let categoryData = {
-          category: this.categoryName,
-          description: this.categoryDescription
-        }
-        if (categoryData) {
-          this.categoryService.addCategory(categoryData)
-            .subscribe((res) => {
-              // console.log('Headers List', res.headers.keys());
-              console.log('Category Added', res);
-              this.categoryName = "";
-              if (res['message'] == "success")
-                resolve(this.utilityService.resolveAsyncPromise('Category added!'));
-              else
-                reject(this.utilityService.rejectAsyncPromise('Category already exist'));
-            }, (err) => {
-              // console.log('Headers List', err.headers.keys());
-              console.log('Category Not added', err);
-              // this.snotifyService.error("Category not added, please try again!");
-              reject(this.utilityService.rejectAsyncPromise('Unable to add the category, please try again'));
-            })
-        }
-      })) 
-    } catch (err) {
-      console.log('Internal Server Error', err);
-      this.snotifyService.error("Some internal server occured, kindly check after some time...");
+    if(this.categoryName!=null && this.categoryName!='' && this.categoryDescription!=null && this.categoryDescription != ''){
+      try {
+        this.utilityService.asyncNotification('Creating new category, please wait...', new Promise((resolve, reject) => {
+          let categoryData = {
+            category: this.categoryName,
+            description: this.categoryDescription
+          }
+          if (categoryData) {
+            this.categoryService.addCategory(categoryData)
+              .subscribe((res) => {
+                // console.log('Headers List', res.headers.keys());
+                console.log('Category Added', res);
+                this.categoryName = "";
+                if (res['message'] == "success")
+                  resolve(this.utilityService.resolveAsyncPromise('Category added!'));
+                else
+                  reject(this.utilityService.rejectAsyncPromise('Category already exist'));
+              }, (err) => {
+                // console.log('Headers List', err.headers.keys());
+                console.log('Category Not added', err);
+                // this.snotifyService.error("Category not added, please try again!");
+                reject(this.utilityService.rejectAsyncPromise('Unable to add the category, please try again'));
+              })
+          }
+        })) 
+      } catch (err) {
+        console.log('Internal Server Error', err);
+        this.snotifyService.error("Some internal server occured, kindly check after some time...");
+      }
+    } else{
+      this.utilityService.warningNotification('Please fill up all the fields!')
     }
+
 
   }
 
